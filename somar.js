3c77ab0bfb1cd197bc033eb61106a5eaf5eb55d3
Bolsarim <142364522+Bolsarim@users.noreply.github.com>
2024-05-28 21:14:58 -0300

    function

--- somar.js
+++ somar.js
@@ -1,14 +1,9 @@
 function somar(a, b) {
-    if (typeof a === 'string') {
-        a = parseInt(a, 10);            
-    }
-    if (typeof b === 'string') {
-        b = parseInt(b, 10);           
-    }
-            
-    if (isNaN(a) || isNaN(b)) {               
-        return "Caractere inválido";                
-    }
+a = parseFloat(a)
+b = parseFloat(b)
+if((isFinite(a) == false) || (isFinite(b) == false)){
+    return "Caractere inválido"
+}
    
     return a + b;
 }
@@ -16,12 +11,4 @@ function somar(a, b) {
 module.exports = somar;
  
 
- /*if (typeof a === 'string') { verifica se o tipo do parâmetro a é uma string.
-a = parseInt(a, 10); se a for uma string, ele é convertido para um número inteiro usando parseInt(a, 10).
-parseInt é uma função nativa do JavaScript que converte uma string em um número inteiro. O segundo argumento 10 especifica a base (radix) da conversão, que neste caso é a base decimal.*/
-
-
-
-/*if (isNaN(a) || isNaN(b)) { verifica se a ou b não são números válidos após a conversão.
-isNaN é uma função que retorna true se o argumento não for um número (NaN - Not-a-Number).
-return "Caractere inválido"; se qualquer um dos parâmetros não for um número válido, a função retorna a string "Caractere inválido".*/
+ 
